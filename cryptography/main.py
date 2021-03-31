10ba747e526521f6397499d1125426de4d831c1f
mac <mac@macs-MacBook-Air.local>
2021-03-31 13:41:31 +0100

    updated changes(error checking)

--- cryptography/main.py
+++ cryptography/main.py
@@ -32,18 +32,27 @@ def cryptography(d_e_code,e_text,key):
 while True:
     print(art)
     #checking for exceptions
-    decode_encode = input("do you want to encode or decode: ")
+    decode_encode = input("type encode to encdoe or decode to decode: ")
     while decode_encode != 'encode' and decode_encode != 'decode':
-        decode_encode = input("error: please typee encode to encode and decode to decode ")
-    enter_text = input("enter text: ")
+        decode_encode = input("error: please type encode to encode and decode to decode ")
+    enter_text = input("enter plain text: ")
     while enter_text.isalpha() == False:
-      enter_text = input("Error: enter text only ")
-    enter_key = input('enter key: ')
+      enter_text = input("Error: enter plaintext only ")
+    enter_key = input('enter encryption key: ')
     while enter_key.isdigit() == False:
-       enter_key = input('Error: enter digits or numbers as your key only ')
+       enter_key = input('Error: use numbers as encription key only ')
 
     if enter_key == "":
         enter_key = 0
+
+
+    print(cryptography(decode_encode,enter_text,enter_key))
+
+    yes_no = input("enter yes to encript again or no to exit: ")
+    if yes_no == 'yes':
+        continue
+    elif yes_no == 'no':
+        break
 
 
     print(cryptography(decode_encode,enter_text,enter_key))
---
+++ cryptography/print(range(1..py
@@ -0,0 +1,2 @@
+b = [range(1,10)]
+print(b)
