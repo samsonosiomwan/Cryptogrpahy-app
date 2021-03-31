a9a107f04b2edc46704b4b442a73a919109dc727
mac <mac@macs-MacBook-Air.local>
2021-03-31 08:45:45 +0100

    first commit

--- cryptography/main.py
+++ cryptography/main.py
@@ -2,8 +2,51 @@ from art import art
 
 
 
-def cryptography():
-  print(art)
+
   
+#algotithm to encrypt and decript
+#keys: d_e_code:decrypt_encrypt_code, e_text:encrypt_text
+def cryptography(d_e_code,e_text,key):
+    '''
+    encrypt function
+    d_e_code:decrypt_encrypt_code, e_text:encrypt_text,key
+    '''
+    transverse = ''
+
+    for text in e_text:
+        if ord(text) > 122 or ord(text) < 96:
+            transverse += text
+        else: 
+            if d_e_code == "encode":
+                transverse_text = ord(text) + int(key)
+                if transverse_text > 122:
+                    transverse_text = (transverse_text % 122) + 96
+                transverse += chr(transverse_text)
+            elif d_e_code == "decode":
+                transverse_text = ord(text) - int(key)
+                if transverse_text < 97:
+                    transverse_text = 122 - (96 - transverse_text)
+                transverse += chr(transverse_text)
+    return f'this is your entry message: {transverse}' 
+        
+while True:
+    print(art)
+    decode_encode = input("do you want to encode or decode: ")
+    while decode_encode != 'encode' and decode_encode != 'decode':
+        decode_encode = input("Error: ")
+    enter_text = input("enter text: ")
+    enter_key = input('enter key: ')
+    if enter_key == "":
+        enter_key = 0
+
+
+    print(cryptography(decode_encode,enter_text,enter_key))
+
+    yes_no = input("enter yes to encript again or no to exit: ")
+    if yes_no == 'yes':
+        continue
+    elif yes_no == 'no':
+        break
+
   
-cryptography()
+# cryptography()
--- dict_comp.py
+++ dict_comp.py
@@ -1,3 +1,21 @@
 
-def dict_comp(stop, step):
-  pass
+
+#dict_comp function to accept step,stop arguments and return a dictionary using comprehension
+
+def dict_comp(stop,step):
+    pass
+    '''input integer_____for step,stop'''
+    #input function assigned to variables to hold stop and step value
+    stop,step=int(input('enter stop value')), int(input('enter step value'))
+    
+    #list comprehension for the stop, step iteration
+    items_list1=[value for  value in range(0,stop) ]
+    items_list2=[items_list1[item1:item1+step]for item1 in range(1,len(items_list1),step)]
+    
+    #dictionary comprehension holding values from stop and step
+    items_dict={f'item_{item1 + 1}':item2 for item1,item2 in zip(items_list1,items_list2)}
+    items_dict.popitem()
+    
+    return items_dict
+
+print(dict_comp(0,1))
